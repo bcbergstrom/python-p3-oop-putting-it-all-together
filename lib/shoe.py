18089ef7af7310525c0c9feee6c9df2d77cc45b4
Benton Bergstrom <bbergstrom0524@gmail.com>
2024-07-16 13:59:22 -0600

    final commit

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = None
+    
+
+    def get_shoe_size(self):
+        return self._size
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+    
+
+    def set_shoe_size(self, count):
+        if count is int:
+            self.size = count
+        else:
+            print("size must be an integer")
+
+    size = property(get_shoe_size, set_shoe_size)
